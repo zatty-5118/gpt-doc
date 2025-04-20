0cf56fd59199874580e922c0ecaef608d2c48894
zatty-5118 <162966571+zatty-5118@users.noreply.github.com>
2025-04-20 20:59:47 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ScrollReveal().reveal('.result-panel', {
+    delay: 100,
+    interval: 150,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-out',
+    duration: 500,
+    reset: false
+});
+ScrollReveal().reveal('.flow-step', {
+    delay: 100,
+    interval: 150,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-out',
+    duration: 500,
+    reset: false
+});
